f31a09efe9216926b07d3306a1c9fef08c48da8e
Ryan Linton <ryanlntn@gmail.com>
2013-04-16 21:51:48 -0700

    add exercise 6.2

--- chapter6.js
+++ chapter6.js
@@ -58,3 +58,16 @@ print(count(equals(1, a)));
 // Remember that strings have a charAt method that can be used to look at a specific character inside them.
 //
 // Ex. 6.2
+
+function processParagraph(paragraph) {
+  var header = 0;
+  while (paragraph.charAt(0) == "%") {
+    paragraph = paragraph.slice(1);
+    header++;
+  }
+
+  return {type: (header == 0 ? "p" : "h" + header),
+          content: paragraph};
+}
+
+show(processParagraph(paragraphs[0]));
